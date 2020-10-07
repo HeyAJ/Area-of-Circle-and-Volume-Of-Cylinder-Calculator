3ac67237d49adbbf0f89ab28f9e108fe81c9d9d7
Aditya <adityavinodjain1234@gmail.com>
2020-10-07 19:27:14 +0530

    Initial Commit

--- AreaofCircleCalculator.c
+++ AreaofCircleCalculator.c
@@ -1,20 +1,27 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     int radius;
     float pie = 3.14;
+    int decision;
 
     printf("please enter the radius of your circle: ");
     scanf("%d", &radius);
 
     printf("the area of your circle is: %f \n", pie * radius * radius);
 
-    int height;
-    printf("please enter the height of the cylinder: ");
-    scanf("%d", &height);
+    printf("Do You want To Find the the volume of a cylinder using the radius that you give above\n For yes press 1\n For No Press 2: ");
+    scanf("%d", &decision);
 
-    printf("the volume of the circle is: %f", pie * radius * radius * height);
+    if (decision == 1)
+    {
+        int height;
+        printf("please enter the height of the cylinder: ");
+        scanf("%d", &height);
 
-    return 0;
+        printf("the volume of the circle is: %f", pie * radius * radius * height);
 
+        return 0;
+    }
 }
